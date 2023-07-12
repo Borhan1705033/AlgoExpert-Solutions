fd92f889664125083b197d538725de4d55fc48f6
Borhan1705033 <67091146+Borhan1705033@users.noreply.github.com>
2023-07-12 14:14:19 +0600

    Add files via upload

---
+++ Insertion Sort.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+vector<int> insertionSort(vector<int> array) {
+  // Write your code here.
+  for(int i=1;i<array.size();i++)
+    {
+      int key = array[i];
+      int j=i-1;
+      while(j>=0 && array[j]>key)
+        {
+          array[j+1]=array[j];
+          j--;
+        }
+      array[j+1]=key;
+    }
+
+  return {array};
+}
+
